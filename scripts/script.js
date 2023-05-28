d7df7030eb0a3eb3869db724dd37e37c694217c8
AndreasAcker <andreas.acker@hotmail.com>
2023-05-28 11:24:45 +0200

    opdatering

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,34 @@
-var counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 5){
-        counter = 1;
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('JavaScript er forbundet og fungerer.');
+  
+    // Resten af din JavaScript-kode herunder
+    // ...
+  });
+  
+document.addEventListener('DOMContentLoaded', function() {
+    var counter = 0;
+    var slides = document.querySelectorAll('.slide');
+  
+    function updateSlideMargin() {
+      var marginLeft = '-' + (counter * 100) + '%';
+      document.querySelector('.slidestart').style.marginLeft = marginLeft;
     }
-}, 5000);
+  
+    setInterval(function() {
+      counter++;
+      if (counter >= slides.length) {
+        counter = 0;
+      }
+      updateSlideMargin();
+    }, 5000);
+  
+    // Lyt efter ændringer i radioknapperne
+    var radioBtns = document.querySelectorAll('input[name="radio-btn"]');
+    radioBtns.forEach(function(radioBtn, index) {
+      radioBtn.addEventListener('change', function() {
+        counter = index;
+        updateSlideMargin();
+      });
+    });
+  });
+  
